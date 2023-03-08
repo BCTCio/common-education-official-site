39ca2aa4bd2881105d0b92c41e93aa3fa1fb6211
mxdi9i7 <peter.zheng88228@gmail.com>
2023-03-08 06:24:31 -0500

    initial

---
+++ src/components/TableOfContents.jsx
@@ -0,0 +1,97 @@
+import { Container } from '@/components/Container'
+import { Expandable } from '@/components/Expandable'
+import { SectionHeading } from '@/components/SectionHeading'
+
+const tableOfContents = {
+  'Getting started': {
+    'Getting started': 1,
+    'Intro to Figma': 15,
+    'Setting up your first artboard': 20,
+  },
+  Fundamentals: {
+    'Strokes and fills': 21,
+    'End points': 22,
+    'Bezier curves': 26,
+    'Designing on a grid': 31,
+    'Vector shapes': 45,
+  },
+  'Boolean operations': {
+    'Combining shapes': 50,
+    'Subtracting shapes': 57,
+    'Intersecting shapes': 66,
+    Flattening: 78,
+  },
+  'Optimizing for production': {
+    'Preparing for SVG': 82,
+    'Configuring your export settings': 88,
+    'Minifying and removing metadata': 95,
+  },
+}
+
+export function TableOfContents() {
+  return (
+    <section
+      id="table-of-contents"
+      aria-labelledby="table-of-contents-title"
+      className="py-16 scroll-mt-14 sm:scroll-mt-32 sm:py-20 lg:py-32"
+    >
+      <Container>
+        <SectionHeading number="1" id="table-of-contents-title">
+          Our Mission
+        </SectionHeading>
+        <p className="mt-8 text-4xl font-bold tracking-tight font-display text-slate-900">
+          Get a look at all of the content covered in the book. Everything you
+          need to know is inside.
+        </p>
+        <p className="mt-4 text-lg tracking-tight text-slate-700">
+          “Everything Starts as a Square” is comprised of 240 tightly edited,
+          highly visual pages designed to teach you everything you need to know
+          about icon design with no unnecessary filler.
+        </p>
+        <Expandable>
+          {({ isExpanded }) => (
+            <>
+              <ol role="list" className="mt-16 space-y-10 sm:space-y-16">
+                {Object.entries(tableOfContents)
+                  .slice(0, isExpanded ? undefined : 2)
+                  .map(([title, pages]) => (
+                    <li key={title}>
+                      <h3 className="text-3xl font-bold tracking-tight font-display text-slate-900">
+                        {title}
+                      </h3>
+                      <ol
+                        role="list"
+                        className="px-6 py-3 mt-8 text-base tracking-tight divide-y divide-slate-300/30 rounded-2xl bg-slate-50 sm:py-7 sm:px-8"
+                      >
+                        {Object.entries(pages).map(([title, pageNumber]) => (
+                          <li
+                            key={title}
+                            className="flex justify-between py-3"
+                            aria-label={`${title} on page ${pageNumber}`}
+                          >
+                            <span
+                              className="font-medium text-slate-900"
+                              aria-hidden="true"
+                            >
+                              {title}
+                            </span>
+                            <span
+                              className="font-mono text-slate-400"
+                              aria-hidden="true"
+                            >
+                              {pageNumber}
+                            </span>
+                          </li>
+                        ))}
+                      </ol>
+                    </li>
+                  ))}
+              </ol>
+              <Expandable.Button>See more</Expandable.Button>
+            </>
+          )}
+        </Expandable>
+      </Container>
+    </section>
+  )
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,84 @@
+import Head from 'next/head'
+
+import { Author } from '@/components/Author'
+import { Footer } from '@/components/Footer'
+import { FreeChapters } from '@/components/FreeChapters'
+import { Hero } from '@/components/Hero'
+import { Introduction } from '@/components/Introduction'
+import { NavBar } from '@/components/NavBar'
+import { Pricing } from '@/components/Pricing'
+import { Resources } from '@/components/Resources'
+import { Screencasts } from '@/components/Screencasts'
+import { TableOfContents } from '@/components/TableOfContents'
+import { Testimonial } from '@/components/Testimonial'
+import { Testimonials } from '@/components/Testimonials'
+import avatarImage1 from '@/images/avatars/avatar-1.png'
+import avatarImage2 from '@/images/avatars/avatar-2.png'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>
+          Common Education Foundation - Empowering Education for All
+        </title>
+        <meta
+          name="description"
+          content="At Common Education Foundation, we give children living in
+          poverty around the world an equal opportunity to receive a quality
+          education. Through public donations, we are able to provide children
+          in need with the necessary resources and support services to ensure
+          that they have the same access to educational opportunities as their
+          more affluent peers. We believe that education is a fundamental right
+          for all children and that no child should be denied the opportunity to
+          reach their full potential. Join us in our mission to ensure that all
+          children have access to a quality education!"
+        />
+      </Head>
+      <Hero />
+      <Introduction />
+      <NavBar />
+      <TableOfContents />
+      <Testimonial
+        id="testimonial-from-tommy-stroman"
+        author={{
+          name: 'Tommy Stroman',
+          role: 'Front-end developer',
+          image: avatarImage1,
+        }}
+      >
+        <p>
+          At Common Education Foundation, we give children living in poverty
+          around the world an equal opportunity to receive a quality education.
+          Through public donations, we are able to provide children in need with
+          the necessary resources and support services to ensure that they have
+          the same access to educational opportunities as their more affluent
+          peers. We believe that education is a fundamental right for all
+          children and that no child should be denied the opportunity to reach
+          their full potential. Join us in our mission to ensure that all
+          children have access to a quality education!
+        </p>
+      </Testimonial>
+      <Screencasts />
+      <Testimonial
+        id="testimonial-from-gerardo-stark"
+        author={{
+          name: 'Gerardo Stark',
+          role: 'Creator of Pandemicons',
+          image: avatarImage2,
+        }}
+      >
+        <p>
+          “I’ve tried to create my own icons in the past but quickly got
+          frustrated and gave up. Now I sell my own custom icon sets online.”
+        </p>
+      </Testimonial>
+      <Resources />
+      <FreeChapters />
+      <Pricing />
+      <Testimonials />
+      <Author />
+      <Footer />
+    </>
+  )
+}
